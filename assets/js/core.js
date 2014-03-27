8b954a3b5edb6ceb09154a5e16b038bcb0de0da1
William Oliveira <sudowilliam@gmail.com>
2014-03-27 03:44:47 -0300

    shadows + stats

--- assets/js/core.js
+++ assets/js/core.js
@@ -10,7 +10,7 @@ var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHei
 // ====================
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setClearColorHex(0xEEEEEE, 1.0);
+renderer.setClearColor(0xEEEEEE, 1.0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMapEnabled = true;
 
@@ -27,6 +27,7 @@ scene.add(axes);
 
 var spotLight = new THREE.SpotLight( 0xffffff );
 spotLight.position.set( -40, 60, -10 );
+spotLight.castShadow = true;
 scene.add( spotLight );
 
 // =======================
@@ -37,10 +38,13 @@ var planeGeometry = new THREE.PlaneGeometry(60,20);
 var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
 var plane = new THREE.Mesh(planeGeometry,planeMaterial);
 
+plane.receiveShadow = true;
+
 plane.rotation.x =- 0.5 * Math.PI;
 plane.position.x = 15;
 plane.position.y = 0;
 plane.position.z = 0;
+
 scene.add(plane);
 
 // ======================
@@ -51,9 +55,12 @@ var cubeGeometry = new THREE.CubeGeometry(4,4,4);
 var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
+cube.castShadow = true;
+
 cube.position.x = -4;
 cube.position.y = 3;
 cube.position.z = 0;
+
 scene.add(cube);
 
 // ========================
@@ -64,9 +71,12 @@ var sphereGeometry = new THREE.SphereGeometry(4,20,20);
 var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x7777ff});
 var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
 
+sphere.castShadow = true;
+
 sphere.position.x = 20;
 sphere.position.y = 4;
 sphere.position.z = 2;
+
 scene.add(sphere);
 
 // ==================
@@ -82,9 +92,22 @@ camera.lookAt(scene.position);
 // Render the scene
 // ============================
 
-document.body.appendChild(renderer.domElement);
+function initStats () {
+   var stats = new Stats();
+   stats.setMode(0);
+   stats.domElement.style.position = 'absolute';
+   stats.domElement.style.left = '0px';
+   stats.domElement.style.top = '0px';
+   document.getElementById('stats').appendChild( stats.domElement );
+
+   return stats;
+};
+
+var stats = initStats();
 
 var render = function () {
+    stats.update();
+
     requestAnimationFrame(render);
 
     sphere.rotation.x += 0.05;
@@ -96,4 +119,5 @@ var render = function () {
     renderer.render(scene, camera);
 };
 
+document.body.appendChild(renderer.domElement);
 render();
