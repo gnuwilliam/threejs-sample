a79814f9d92b9f3c8192ff4c2710101f8529a325
William Oliveira <sudowilliam@gmail.com>
2014-03-27 03:21:57 -0300

    new example

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,23 +1,85 @@
+// ============================
+// Defines the camera and scene
+// ============================
+
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.3, 1000 );
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// ====================
+// Defines the renderer
+// ====================
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.querySelector('.container').appendChild( renderer.domElement );
+renderer.setClearColorHex(0xEEEEEE);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// ================
+// Defines the axes
+// ================
 
-camera.position.z = 5;
+var axes = new THREE.AxisHelper( 20 );
+scene.add(axes);
 
-function render() {
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
+// =====================
+// Defines the spotlight
+// =====================
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.04;
-}
+var spotLight = new THREE.SpotLight( 0xffffff );
+spotLight.position.set( -40, 60, -10 );
+scene.add( spotLight );
 
-render();
+// =======================
+// Defines a plane element
+// =======================
+
+var planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
+var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
+var plane = new THREE.Mesh(planeGeometry,planeMaterial);
+
+plane.rotation.x =- 0.5 * Math.PI;
+plane.position.x = 15;
+plane.position.y = 0;
+plane.position.z = 0;
+scene.add(plane);
+
+// ======================
+// Defines a cube element
+// ======================
+
+var cubeGeometry = new THREE.CubeGeometry(4,4,4);
+var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+cube.position.x = -4;
+cube.position.y = 3;
+cube.position.z = 0;
+scene.add(cube);
+
+// ========================
+// Defines a sphere element
+// ========================
+
+var sphereGeometry = new THREE.SphereGeometry(4,20,20);
+var sphereMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
+var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+
+sphere.position.x = 20;
+sphere.position.y = 4;
+sphere.position.z = 2;
+scene.add(sphere);
+
+// ==================
+// Camera definitions
+// ==================
+
+camera.position.x = -30;
+camera.position.y = 40;
+camera.position.z = 30;
+camera.lookAt(scene.position);
+
+// ============================
+// Render the scene
+// ============================
+
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
